7cfe48f5a477301c9277e770fe35377ff2c447de
naveenchandrasekar <56059631+naveenchandrasekar@users.noreply.github.com>
2022-08-28 21:30:39 +0530

    Add files via upload

---
+++ Product Website/src/data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixel 6- Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "GOOGLE",
+    info:
+      "Google Pixel 6 mobile was launched on 19th October 2021. The phone comes with a 90 Hz refresh rate 6.40-inch touchscreen display offering a resolution of 1080x2400 pixels at a pixel density of 411 pixels per inch (ppi) and an aspect ratio of 20:9. Google Pixel 6 is powered by a 2.8GHz octa-core Google Tensor processor that features 2 cores clocked at 2.8GHz and 2 cores clocked at 2.25GHz. It comes with 8GB of RAM. The Google Pixel 6 runs Android 12 and is powered by a 4614mAh battery. The Google Pixel 6 supports wireless charging, as well as proprietary fast charging.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 2,
+    title: "Samsung Note 10",
+    img: "img/product-2.png",
+    price: 20,
+    company: "SAMSUNG",
+    info:
+      "Samsung Galaxy Note 10 mobile was launched on 7th August 2019. The phone comes with a 6.30-inch touchscreen display offering a resolution of 1080x2280 pixels at a pixel density of 401 pixels per inch (ppi) and an aspect ratio of 19:9. Samsung Galaxy Note 10 is powered by a 1.9GHz octa-core Samsung Exynos 9825 processor that features 4 cores clocked at 1.9GHz, 2 cores clocked at 2.4GHz and 2 cores clocked at 2.7GHz. It comes with 8GB of RAM. The Samsung Galaxy Note 10 runs Android 9 Pie and is powered by a 3500mAh battery. The Samsung Galaxy Note 10 supports wireless charging.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 3,
+    title: "Redmi Note 10 pro - Black",
+    img: "img/product-3.png",
+    price: 5,
+    company: "Xiomi",
+    info:
+      "Redmi Note 10 Pro mobile was launched on 4th March 2021. The phone comes with a 6.67-inch touchscreen display. The display sports Gorilla Glass for protection. Redmi Note 10 Pro is powered by an octa-core Qualcomm Snapdragon 732G processor. It comes with 6GB, 8GB of RAM. The Redmi Note 10 Pro runs Android 11 and is powered by a 5050mAh battery. The Redmi Note 10 Pro supports proprietary fast charging.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 4,
+    title: "Motorola edge 30 pro - Black",
+    img: "img/product-4.png",
+    price: 18,
+    company: "Motorola",
+    info:
+      "The Motorola Edge 30 Pro is the first smartphone in the new Edge 30 series. It sports a massive 6.7-inch full-HD+ AMOLED display with a maximum refresh rate of 144Hz. Motorola has added stereo speakers into the mix, which will improve the overall content watching experience. The Edge 30 Pro has a glass back that makes it feel premium in the hand. However, it has a plastic frame that feels odd. It weighs 196g which is definitely noticeable, and its IP52 rated body is splash resistant.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 5,
+    title: "Realme 9 Pro 5G",
+    img: "img/product-5.png",
+    price: 14,
+    company: "Realme",
+    info:
+      "Realme 9 Pro 5G mobile was launched on 16th February 2022. The phone comes with a 120 Hz refresh rate 6.60-inch touchscreen display offering a resolution of 1080x2412 pixels. Realme 9 Pro 5G is powered by an octa-core Qualcomm Snapdragon 695 processor. It comes with 6GB, 8GB of RAM. The Realme 9 Pro 5G runs Android 12 and is powered by a 5000mAh battery. The Realme 9 Pro 5G supports Dash Charge fast charging.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 6,
+    title: "Iphone 13 Pro Max",
+    img: "img/product-6.png",
+    price: 38,
+    company: "Apple",
+    info:
+      "The iPhone 13 Pro Max is Apple's biggest iPhone, but it doens't have any advantages over the iPhone 13 Pro in terms of features, unlike in previous generations. It's heavy and unwieldy, but the big display is great for games and the increased battery capacity allows for nearly two full days of regular use. The display gets a 120Hz refresh rate, but there's still an unsightly notch that interferes with movies. Performance is great thanks to the A15 Bionic SoC. You can choose betweeen 128GB, 256GB, 512GB and 1TB storage options. The main new features are camera-related: Cinematic Mode and Photographic Styles. The camera hardware has also been improved, compared to the previous generation. You can now take macros and the optical zoom goes up to 3X. Although incredibly expensive, the iPhone 13 Pro Max represents the best of the best in the iPhone lineup.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 7,
+    title: "Iphone 12 Pro",
+    img: "img/product-7.png",
+    price: 26,
+    company: "Apple",
+    info:
+      "The iPhone 12 and iPhone 12 Mini (stylized as iPhone 12 mini) are smartphones designed, developed, and marketed by Apple Inc. They are the fourteenth-generation, lower-priced iPhones, succeeding the iPhone 11. They were unveiled at an Apple Special Event, held virtually, on October 13, 2020 alongside the higher-end iPhone 12 Pro and iPhone 12 Pro Max flagships at Apple Park in Cupertino, California. Pre-orders for the iPhone 12 started on October 16, 2020, and the phone became available in most countries on October 23, 2020. Pre-orders for the iPhone 12 Mini began on November 6, 2020, and the phone became available on November 13, 2020.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 8,
+    title: "One Plus 9 Pro",
+    img: "img/product-8.png",
+    price: 21,
+    company: "OnePus",
+    info:
+      "OnePlus hasn't gone too crazy with the design of the OnePlus 9 Pro. It uses a 6.7-inch LTPO OLED display which the company claims cuts power consumption by up to 50 percent compared to the previous model. It's powered by the Qualcomm Snapdragon 888 SoC, and is available in two varaints. I was happy with the accuracy of the in-display fingerprint sensor, and I also like the fact that the edges of the display are slightly less curved compared to the OnePlus 8 Pro, which helps in preventing accidental touches",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel 6- Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Google Pixel 6 mobile was launched on 19th October 2021. The phone comes with a 90 Hz refresh rate 6.40-inch touchscreen display offering a resolution of 1080x2400 pixels at a pixel density of 411 pixels per inch (ppi) and an aspect ratio of 20:9. Google Pixel 6 is powered by a 2.8GHz octa-core Google Tensor processor that features 2 cores clocked at 2.8GHz and 2 cores clocked at 2.25GHz. It comes with 8GB of RAM. The Google Pixel 6 runs Android 12 and is powered by a 4614mAh battery. The Google Pixel 6 supports wireless charging, as well as proprietary fast charging.",
+  inCart: false,
+  count: 0,
+  total: 0,
+};
